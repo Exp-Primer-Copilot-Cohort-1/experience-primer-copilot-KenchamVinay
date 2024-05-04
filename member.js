6f513caf73f727a5921c1db3d141bbb47945a0d7
Kencham Vinay <kenchamvinay.cse2020@citchennai.net>
2024-05-04 14:01:14 +0000

    second copolit commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    // Add the skills to the member
+    var member = {
+        name: 'John Doe',
+        skills: ['JavaScript', 'React', 'Node']
+    };
+
+    return member;
+}
